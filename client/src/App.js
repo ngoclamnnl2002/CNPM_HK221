1916ae7c9f813eb7b06b7e301d298db898087d57
Nguyễn Ngọc Lâm <lamnguyenngoc@246.3.168.191.isp.timbrasil.com.br>
2022-11-21 16:07:50 +0700

    header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import Test from "./pages/tests";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Test />}/>
       <Route path="/dang-nhap" element={<LoginPage />}/>
     </Routes>
   );
---
+++ client/src/pages/tests/index.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+
+const Test = () => {
+    return (
+        <>
+        <Header />
+        </>
+    )
+}
+
+export default Test;
